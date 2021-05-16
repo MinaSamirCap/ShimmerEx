ea84d50a37aa813fdb072cf3617d8dc9bad056f4
MinaSamir <menasamercap@gmail.com>
2021-05-16 03:30:16 +0200

    add shimmer view

--- app/src/main/java/com/mmd/shimmerex/MainActivity.kt
+++ app/src/main/java/com/mmd/shimmerex/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.mmd.shimmerex
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.facebook.shimmer.ShimmerFrameLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val shimmerLayout = findViewById<ShimmerFrameLayout>(R.id.shimmer_layout)
+        shimmerLayout.startShimmer()
     }
 }
